297347e027546583c76b110c5d95d1d84b582f4c
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-09-02 19:03:20 +0900

    Project setup

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace subtitles_maker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
